705f129b56ae8964647a6e0e0d5e81e118a132c5
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-17 15:05:19 +0300

    Add data another text file

--- text-detection/index.js
+++ text-detection/index.js
@@ -1,15 +1,16 @@
 import fs from 'fs';
 import csv from 'csv-parser';
 
-const arananKelime = "react";
+const arananKelime = "devops";
 const dosyaYolu = 'jobs.csv';
+const sonucDosyaYolu = 'result.txt';
 
+const yazmaAkisi = fs.createWriteStream(sonucDosyaYolu);
 const okumaAkisi = fs.createReadStream(dosyaYolu);
 
 okumaAkisi
   .pipe(csv())
   .on('data', (row) => {
-    // Verilerde aranan kelimeyi içeren satırları bulun
     const jobTitle = row['Job Title'] || 'Bilgi Yok';
     const jobExperience = row['Job Experience Required'] || 'Bilgi Yok';
     const keySkills = row['Key Skills'] || 'Bilgi Yok';
@@ -21,26 +22,15 @@ okumaAkisi
     const longitude = row['Longitude'] || 'Bilgi Yok';
     const latitude = row['Latitude'] || 'Bilgi Yok';
     const sal = row['sal'] || 'Bilgi Yok';
-
     if (jobTitle.toLowerCase().includes(arananKelime.toLowerCase()) || keySkills.toLowerCase().includes(arananKelime.toLowerCase())) {
-      console.log('Job Title:', jobTitle);
-      console.log('Job Experience Required:', jobExperience);
-      console.log('Key Skills:', keySkills);
-      console.log('Role Category:', roleCategory);
-      console.log('Location:', location);
-      console.log('Functional Area:', functionalArea);
-      console.log('Industry:', industry);
-      console.log('Role:', role);
-      console.log('Longitude:', longitude);
-      console.log('Latitude:', latitude);
-      console.log('sal:', sal);
-      console.log('---');
+      const satir = `Job Title: ${jobTitle}\nJob Experience Required: ${jobExperience}\nKey Skills: ${keySkills}\nRole Category: ${roleCategory}\nLocation: ${location}\nFunctional Area: ${functionalArea}\nIndustry: ${industry}\nRole: ${role}\nLongitude: ${longitude}\nLatitude: ${latitude}\nsal: ${sal}\n---\n`;
+      yazmaAkisi.write(satir);
     }
   })
   .on('end', () => {
     console.log('Arama tamamlandı.');
+    yazmaAkisi.end();
   });
-
 
 
 
