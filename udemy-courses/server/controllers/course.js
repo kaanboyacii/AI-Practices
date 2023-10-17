0199d89a1025145a299c2152e6b92c284744053d
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-17 21:30:31 +0300

    Func for get courses by word

--- udemy-courses/server/controllers/course.js
+++ udemy-courses/server/controllers/course.js
@@ -1,31 +1,36 @@
-import csvtojsonV2 from 'csvtojson';
 import fs from 'fs';
+import csvParser from 'csv-parser';
 
-const arananKelime = "Financial";
 const dosyaYolu = './data/udemy_courses.csv';
 
-export const getCoursesByWord = async (req, res, next) => {
+export const getCoursesByWord = (req, res, next) => {
   try {
+    const { word } = req.params; 
+
+    if (!word) {
+      return res.status(400).json({ error: 'Aranan kelime eksik.' });
+    }
+
     const kurslar = [];
 
     fs.createReadStream(dosyaYolu)
-      .pipe(csvtojsonV2())
+      .pipe(csvParser())
       .on('data', (row) => {
         const courseTitle = row['course_title'] || '';
-        if (courseTitle.toLowerCase().includes(arananKelime.toLowerCase())) {
+        if (courseTitle.toLowerCase().includes(word.toLowerCase())) {
           kurslar.push({
-            course_id: row.course_id,
-            course_title: row.course_title,
-            url: row.url,
-            is_paid: row.is_paid,
-            price: row.price,
-            num_subscribers: row.num_subscribers,
-            num_reviews: row.num_reviews,
-            num_lectures: row.num_lectures,
-            level: row.level,
-            content_duration: row.content_duration,
-            published_timestamp: row.published_timestamp,
-            subject: row.subject,
+            course_id: row['course_id'],
+            course_title: row['course_title'],
+            url: row['url'],
+            is_paid: row['is_paid'],
+            price: row['price'],
+            num_subscribers: row['num_subscribers'],
+            num_reviews: row['num_reviews'],
+            num_lectures: row['num_lectures'],
+            level: row['level'],
+            content_duration: row['content_duration'],
+            published_timestamp: row['published_timestamp'],
+            subject: row['subject'],
           });
         }
       })
