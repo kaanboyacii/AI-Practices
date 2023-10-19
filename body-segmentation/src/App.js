8bc2de0823221f3a85283ff316598f507ffb6d1b
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-19 13:39:34 +0300

    body segmentation in action

--- body-segmentation/src/App.js
+++ body-segmentation/src/App.js
@@ -5,12 +5,55 @@ import * as bodyPix from "@tensorflow-models/body-pix"
 import Webcam from "react-webcam"
 
 function App() {
-  const WebcamRef = useRef(null);
+  const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  const runBodysegment = async () => {
+    const net = await bodyPix.load();
+    console.log("BodyPix model loaded.");
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const person = await net.segmentPersonParts(video);
+      console.log(person);
+
+      const coloredPartImage = bodyPix.toColoredPartMask(person);
+      const opacity = 0.7;
+      const flipHorizontal = false;
+      const maskBlurAmount = 0;
+      const canvas = canvasRef.current;
+
+      bodyPix.drawMask(
+        canvas,
+        video,
+        coloredPartImage,
+        opacity,
+        maskBlurAmount,
+        flipHorizontal
+      );
+    }
+  };
+
+  runBodysegment();
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam ref={WebcamRef} style={{
+        <Webcam ref={webcamRef} style={{
           position: "absolute",
           marginLeft: "auto",
           marginRight: "auto",
