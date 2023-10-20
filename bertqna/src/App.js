ab97aaf97c552bb08734352ed44fc54dc44dcb3a
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-20 12:15:05 +0300

    load tensorflow model

--- bertqna/src/App.js
+++ bertqna/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useRef, useEffect, useState} from 'react';
+import * as tf from "@tensorflow/tfjs"
+import * as qna from "@tensorflow-models/qna"
+import Loader from "react-loader-spinner";
+import { Fragment } from 'react';
 
 function App() {
+  const passageRef = useRef(null);
+  const questionRef = useRef(null);
+  const [model, setModel] = useState(null);
+  const [answer, setAnswer] = useState();
+
+  const loadModel = async () =>{
+    const loadedModel = await qna.load();
+    setModel(loadedModel);
+    console.log('Model Loaded')
+  }
+  useEffect(()=>{loadModel()}, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  
       </header>
     </div>
   );
