08c9a17084f7fa795d25bd608d1b78f6209ba571
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-10-21 13:50:15 +0300

    dependencies & setup webcam & canvas, refs

--- pose-estimation/src/App.js
+++ pose-estimation/src/App.js
@@ -1,9 +1,45 @@
 import './App.css';
 
+import React, { useRef } from 'react';
+import * as tf from "@tensorflow/tfjs"
+import * as posenet from "@tensorflow-models/posenet"
+import Webcam from 'react-webcam';
+
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
       <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
