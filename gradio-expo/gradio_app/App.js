867861fd70cc62f9f45066ffaae08fb71c6ecbbf
kaanboyacii <98668706+kaanboyacii@users.noreply.github.com>
2023-12-25 15:25:37 +0300

    add microphone and camera permissions

--- gradio-expo/gradio_app/App.js
+++ gradio-expo/gradio_app/App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { StatusBar } from 'expo-status-bar';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [cameraPermission, setCameraPermission] = useState(null);
+  const [microphonePermission, setMicrophonePermission] = useState(null);
+  const [permissionMessage, setPermissionMessage] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setCameraPermission(cameraStatus.status === 'granted');
+
+      const microphoneStatus = await Camera.requestMicrophonePermissionsAsync();
+      setMicrophonePermission(microphoneStatus.status === 'granted');
+    })();
+  }, []);
+
+  const askForPermissions = async () => {
+    const cameraStatus = await Camera.requestCameraPermissionsAsync();
+    setCameraPermission(cameraStatus.status === 'granted');
+
+    const microphoneStatus = await Camera.requestMicrophonePermissionsAsync();
+    setMicrophonePermission(microphoneStatus.status === 'granted');
+
+    updatePermissionMessage();
+  };
+
+  const updatePermissionMessage = () => {
+    if (cameraPermission && microphonePermission) {
+      setPermissionMessage('İzinler verildi');
+    } else {
+      setPermissionMessage('İzinler reddedildi');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://imageomics-bioclip-demo.hf.space/--replicas/8mbcf/' }} style={{ flex: 1 }} />
-      <StatusBar style="auto" />
+      {cameraPermission === null || microphonePermission === null ? (
+        <Text>İzin bekleniyor...</Text>
+      ) : (
+        <View style={{ flex: 1 }}>
+          <WebView source={{ uri: 'https://imageomics-bioclip-demo.hf.space/--replicas/8mbcf/' }} style={{ flex: 1 }} />
+          <Text>{permissionMessage}</Text>
+          <Button title="İzinleri Kontrol Et" onPress={askForPermissions} />
+          <StatusBar style="auto" />
+        </View>
+      )}
     </View>
   );
 }
